2858002ec86cc9aefc67698703ff4dea53da5a22
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-06-22 16:52:57 +0900

    Add positioned map

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var map;
+
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: 13
+  });
+  var tribeca = {lat: 40.719526, lng: -74.0089934}
+  var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'First Marker!'
+  });
+  var infowindow = new google.maps.InfoWindow({
+    content: 'Do you ever feel like an Infowindow, ha?'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
